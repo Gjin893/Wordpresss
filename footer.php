92c7404390222f6637288d5f8f3e94fbe9f43376
Gjin893 <gjinibq@gmail.com>
2026-01-19 20:39:46 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,3 +1,38 @@
+<footer class="site-footer">
+    <div class="container">
+        <nav class="footer-menu">
+<?php wp_nav_menu(array('theme_location'->'wp_devs_footer_menu','depth'=>1)); ?>
+        </nav>
+
+    </div>
+
+</footer>
+
+
+
 <?php wp_footer(); ?>
 </body>
 </html>
+<?php
+if(!is_page('landing-page')):?>
+<section class="menu-area">
+
+<div class="container">
+
+
+<nav class="main-menu">
+    <button class ="check-button">
+        <div class="menu-icon">
+            <div class="bar1"></div>
+              <div class="bar2"></div>
+                <div class="bar3"></div>
+        </div>
+
+    </button>
+    <?php wp_nav_menu(array('theme_location'->'wp_devs_main_menu','depth'=>1)); ?>
+
+</nav>
+
+</div>
+
+</section>
