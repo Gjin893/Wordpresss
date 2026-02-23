76d211631af4805948824e52830bc0605a750f2f
unknown <Student@DIGITALSCHOOL.LOCAL>
2026-02-23 20:25:41 +0100

    moduli 13

---
+++ Wordpresss/parts/content-search.php
@@ -0,0 +1,26 @@
+<article id="post- <?php the_ID();?>"<?php post_class();?>>
+    <header>
+        <h2><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+        <?php if('post'== get_post_type()):?>
+
+            <div class="meta-info">
+                <p>Posted in<?php echo get_the_date(); by ?><? the_author_posts_link();?></p>
+           
+
+            <?php if(has_category()):?>
+                Categories: <span><?php the_category('');?> </span>
+                <?php endif;?>
+
+                
+            <?php if(has_tag()):?>
+                Categories: <span><?php the_tag('');?> </span>
+                <?php endif;?> 
+            </div>
+            <?php endif;?>
+    </header>
+<div class="content">
+    <?php the_excerpt();?>
+</div>
+
+
+</article>
